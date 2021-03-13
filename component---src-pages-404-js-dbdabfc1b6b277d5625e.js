8de85eedce41aa69968f931e669a4665cad4ad4b
elit-altum <elit-altum@users.noreply.github.com>
2021-03-13 13:37:38 +0000

    deployed via Gatsby Publish Action 🎩 for 33b8fe5050720e19488c624606ecfedbe65c0c3f

---
+++ component---src-pages-404-js-dbdabfc1b6b277d5625e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),s=a("cQ16");t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{className:"page"},l.a.createElement("div",{className:"container"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-dbdabfc1b6b277d5625e.js.map
